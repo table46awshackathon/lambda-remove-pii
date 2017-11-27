ebaa030793652c08574a6825d768e191e864dad3
Gabriel Nicolae <g.nicolae@samsung.com>
2017-11-27 15:13:39 -0800

    Remove some columns from the CSV

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const _ = require('lodash');
 const aws = require('aws-sdk');
 const s3 = new aws.S3({apiVersion: '2006-03-01', region: 'us-west-2'});
+var csv = require('csv');
 
 exports.handler = function(event, context, callback) {
   if ('Records' in event) {
@@ -33,6 +34,7 @@ function process(record) {
 }
 
 function loadData(bucket, key) {
+  console.log(`Load csv from ${bucket} :: ${key}`);
   const params = {
       Bucket: bucket,
       Key: key,
@@ -41,11 +43,27 @@ function loadData(bucket, key) {
 }
 
 function convertData(data) {
-  return Promise.resolve("TEST\n" + data);
+  var fieldsToRemove = ['First_Name', 'Last_Name'];
+  console.log(`Removing fields from csv: ${fieldsToRemove}`);
+  return new Promise((resolve, reject) => {
+    csv.parse(data.Body.toString(), {columns: true}, function(err, data) {
+      if (err) {
+        return reject(err);
+      }
+      data = _.map(data, (row) => _.omit(row, fieldsToRemove));
+      csv.stringify(data, {header: true}, function(err, data) {
+        if (err) {
+          return reject(err);
+        }
+        resolve(data);
+      });
+    });
+  });
 }
 
 function saveData(bucket, key, data) {
   key = key.replace(/^pii\//, 'clean/');
+  console.log(`Save csv to ${bucket} :: ${key}`);
   const params = {
       Bucket: bucket,
       Key: key,
